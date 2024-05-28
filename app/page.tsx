070d4bd244116bb75c7a0e51409d80d8dd200c47
ramrachai <ramrachaim@gmail.com>
2024-05-28 19:11:52 +0600

    - installed nextui and react-icons. - configured and working

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { Button } from '@nextui-org/react';
+import Image from 'next/image';
+import { FaSmile } from 'react-icons/fa';
 
 export default function Home() {
-  return (
-      <main>
-          <h2>Hello Next match</h2>
-      </main>
-  );
+    return (
+        <main>
+            <h2>Hello Next match</h2>
+            <Button
+                startContent={<FaSmile size={30} />}
+                color="primary"
+                variant="bordered">
+                Solid
+            </Button>
+        </main>
+    );
 }
